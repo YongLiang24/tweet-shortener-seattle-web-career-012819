2182c9144018abfba13fe1de86c0c92257d95db1
yong liang <yongliang24@gmail.com>
2019-01-21 10:18:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,7 +32,7 @@ def bulk_tweet_shortener(str)
 end
 
 def selective_tweet_shortener(tweet)
-  tweet.map do |phrase|
+  tweet.split(" ").map do |phrase|
     if phrase.length > 140
       word_substituter(phrase)
     elsif phrase.length <= 140
